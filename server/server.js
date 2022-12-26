cb26e3b6cfce43324f9eade9cfc17dc6cbeaa592
GargDivanshu <divanshu.garg113@gmail.com>
2022-12-27 01:06:52 +0530

    first

---
+++ server/server.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import {Configuration, OpenAIApi} from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration); 
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.get('/', async (req, res)=> {
+    res.status(200).send({
+        message: 'Hello from CodeAI', 
+    })
+});
+
+app.post('/', async (req, res)=>{
+    try{
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.7,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch(error){
+        console.log(error); 
+        res.status(500).send({ error })
+    }
+})
+
+
+app.post('/image', async (req, res) => {
+  try {
+  const prompt = req.body.prompt;
+
+  const response = await openai.createImage({
+    prompt: `${prompt}`,
+    n: 1,
+    size: "1024x1024",
+  });
+  image_url = response.data.data[0].url;
+  } catch(err) {
+    console.log(err);
+    req.status(500).send({err})
+  }
+})
+
+app.listen(5000, ()=> {
+    console.log('server is on 5000')
+})
