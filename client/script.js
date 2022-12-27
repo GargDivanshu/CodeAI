6bd04ce22622d189ac40063ef79a9b411d9c8de0
GargDivanshu <divanshu.garg113@gmail.com>
2022-12-27 23:45:06 +0530

    final

--- client/script.js
+++ client/script.js
@@ -81,13 +81,14 @@ const messageDiv = document.getElementById(uniqueId);
 loader(messageDiv);
 
 
-const response = await fetch('https://codeai-76wv.onrender.com/', {
+const response = await fetch('http://localhost:5000', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
     },
     body: JSON.stringify({
         prompt: data.get('prompt')
+        
     })
 })
 
--- server/server.js
+++ server/server.js
@@ -1,66 +1,55 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import {Configuration, OpenAIApi} from 'openai';
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
+const result=dotenv.config();
+if (result.error) {
+  throw result.error
+}  
+else {
+console.log(result.parsed) }
+
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-})
-
-const openai = new OpenAIApi(configuration); 
-
-const app = express();
-app.use(cors())
-app.use(express.json());
-
-app.get('/', async (req, res)=> {
-    res.status(200).send({
-        message: 'Hello from CodeAI', 
-    })
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.post('/', async (req, res)=>{
-    try{
-        const prompt = req.body.prompt;
+const openai = new OpenAIApi(configuration);
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0.7,
-            max_tokens: 256,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-        });
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    } catch(error){
-        console.log(error); 
-        res.status(500).send({ error })
-    }
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
 })
 
-
-app.post('/image', async (req, res) => {
+app.post('/', async (req, res) => {
   try {
-  const prompt = req.body.prompt;
+    const prompt = req.body.prompt;
+    console.log(prompt)
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
 
-  const response = await openai.createImage({
-    prompt: `${prompt}`,
-    n: 1,
-    size: "1024x1024",
-  });
-  image_url = response.data.data[0].url;
-  } catch(err) {
-    console.log(err);
-    req.status(500).send({err})
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
   }
 })
 
-app.listen(5000, ()=> {
-    console.log('server is on 5000')
-})
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
